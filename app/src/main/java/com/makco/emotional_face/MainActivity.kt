3b72db64641e6f311c1f78e7ed31ab535833de2b
Makar Kalancha <makarkalancha>
2024-03-05 21:55:15 -0500

    xml

--- app/src/main/java/com/makco/emotional_face/MainActivity.kt
+++ app/src/main/java/com/makco/emotional_face/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.makco.emotional_face
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,12 +11,7 @@ import androidx.compose.ui.Modifier
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val textView = TextView(this)
-        textView.text = "Hello Custom View"
-        setContentView(textView)
-
-
+        setContentView(R.layout.activity_main)
     }
 }
 
